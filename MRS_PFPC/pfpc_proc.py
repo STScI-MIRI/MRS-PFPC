1b2e4dd8c51827c9ce40819562d0cd654017a014
Karl Gordon <kgordon@stsci.edu>
2026-07-28 10:34:33 -0400

    override the flat field

--- MRS_PFPC/pfpc_proc.py
+++ MRS_PFPC/pfpc_proc.py
@@ -157,10 +157,11 @@ def main():
     print(ratefiles)
 
     if dospec2:
-        for file in ratefiles:
+        for file in ratefiles[0:1]:
             asnfile = os.path.join(output_dir, "l2asn.json")
             writel2asn(file, None, ratefiles, asnfile, "Level2")
-            runspec2(asnfile, output_dir, badpix_selfcal=badpix_selfcal)
+            runspec2(asnfile, output_dir, badpix_selfcal=badpix_selfcal,
+                     flatfile="MIRI_FM_MIRIFUSHORT_12SHORT_FLAT_2026TEST.fits")
     else:
         print("Skipping Spec2 processing")
 
--- MRS_PFPC/utils/mrs_helpers.py
+++ MRS_PFPC/utils/mrs_helpers.py
@@ -34,9 +34,13 @@ def rundet1(filename, outdir, showers=True):
     )
 
 
-def runspec2(filename, outdir, nocubes=False, badpix_selfcal=True):
+def runspec2(filename, outdir, nocubes=False, badpix_selfcal=True,
+             flatfile=None,
+             ):
     sp2_dict = {}
     # sp2_dict["fringe"] = {"skip": True}
+    if flatfile is not None:
+        sp2_dict["flat_field"] = {"overide_flat": flatfile}
     sp2_dict["residual_fringe"] = {"skip": True}
     sp2_dict["straylight"] = {"clean_showers": True}
     if badpix_selfcal:
